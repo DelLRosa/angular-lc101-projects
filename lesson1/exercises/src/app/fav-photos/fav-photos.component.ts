d9957cd491785c0ce8fcd3d06c1dd2e4910eed25
Delaney <delaney.rosa1@gmail.com>
2023-03-09 07:53:58 -0600

    commit 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'my cat';
+  image1 = 'https://i.imgur.com/TDTfT1x.jpg';
+  image2 = 'https://i.imgur.com/15HFQXP.jpg';
+  image3 = 'https://i.imgur.com/RUb3VEA.jpg';
 
   constructor() { }
 
